a830dc1fe58de54b4a54741b2edf4d6628740f49
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-04-04 19:00:10 +0200

    aze

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 setInterval(function() {
-    location.reload();
-}, 1000);
+    afficherDonnees();
+}, 3000);
+
+
+function afficherDonnees() {
+    const cheminFichier = 'text.txt';
+
+    fetch(cheminFichier)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la récupération du fichier');
+            }
+            return response.text();
+        })
+        .then(data => {
+            const dataContainer = document.getElementById('dataContainer');
+            dataContainer.textContent = data;
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+        });
+}
