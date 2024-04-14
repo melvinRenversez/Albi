741aa57729616806e5d1ed47d554bd466c3af5fc
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-04-14 18:56:11 +0200

    protodata

--- data/proto/proto_get_data.php
+++ data/proto/proto_get_data.php
@@ -1,8 +1,8 @@
 <?php
 // Connexion à la base de données et récupération des données
-$bdd = new PDO('sqlite:mesures.db');
-$requete = $bdd->query('SELECT * FROM mes_mesures');
-$donnees = $requete->fetch(PDO::FETCH_ASSOC);
+$bdd = new PDO('sqlite:../database.db');
+$requete = $bdd->query('SELECT * FROM Proto');
+$donnees = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 // Retourner les données au format JSON
 header('Content-Type: application/json');
