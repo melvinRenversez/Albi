8afed0f009b7000f0ed3f0d6ab974fd39f640c90
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-04-14 19:33:45 +0200

    urbandata

--- urban/short_run/script.js
+++ urban/short_run/script.js
@@ -1,49 +1,51 @@
 var carMarker;
     
 var mymap = L.map('mapid').setView([51.505, -0.09], 15);
-    
-var carLatitude = 43.913231;
-var carLongitude = 2.113370;
 
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Contributeurs OpenStreetMap</a>'
-}).addTo(mymap);
-    
-function updateCarPosition(latitude, longitude) {
-    if (carMarker) {
-        mymap.removeLayer(carMarker);
-    }
-
-    carMarker = L.marker([latitude, longitude]).addTo(mymap);
-
-    mymap.setView([latitude, longitude], mymap.getZoom());
-}
-
-function refreshData() {
-    $.ajax({
-        url: 'data/get_data.php',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var latitude = data[i].latitude;
-                var longitude = data[i].longitude;
-                console.log(latitude, longitude)
-                updateCarPosition(latitude, longitude)
-            }
-        },
-        error: function(xhr, status, error) {
-            console.error('Erreur lors de la récupération des données: ' + status + ' ' + error);
+document.addEventListener('DOMContentLoaded', function() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Contributeurs OpenStreetMap</a>'
+    }).addTo(mymap);
+        
+    function updateCarPosition(latitude, longitude) {
+        if (carMarker) {
+            mymap.removeLayer(carMarker);
         }
-    });
-}
-
-document.addEventListener('keydown', (e)=>{
-    if(e.keyCode == 8){
-        window.location.href ="../urban.html"
+    
+        carMarker = L.marker([latitude, longitude]).addTo(mymap);
+    
+        mymap.setView([latitude, longitude], mymap.getZoom());
     }
-})
+    
+    function refreshData() {
+        console.log('Refreshing data')
+        $.ajax({
+            url: '../../data/urban/urban_get_data.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var latitude = data[i].latitude;
+                    var longitude = data[i].longitude;
+                    $('#intensiteValue').text(data[i].Intensité)
+                    $('#VInstantaneValue').text(data[i].VInstantanée)
+                    $('#VMoyenneValue').text(data[i].VMoyenne)
+                    updateCarPosition(latitude, longitude)
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Erreur lors de la récupération des données: ' + status + ' ' + error);
+            }
+        });
+    }
+    
+    document.addEventListener('keydown', (e)=>{
+        if(e.keyCode == 8){
+            window.location.href ="../urban.html"
+        }
+    })
+    setInterval(() => {
+        refreshData();
+    }, 500);
+});
 
-
-updateCarPosition(carLatitude, carLongitude);
